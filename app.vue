1e92fb36460f6dd8913e69ecc3039312bf9accb1
zi3lak <zi3lak@gmail.com>
2024-03-24 10:13:25 +0000

    Update app.vue

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     <h1>Piotr Klepuszewski</h1>
   </div>
 
-  <!-- TradingView Widget BEGIN -->
+<!-- TradingView Widget BEGIN -->
 <div class="tradingview-widget-container">
   <div class="tradingview-widget-container__widget"></div>
   <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
