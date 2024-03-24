6f733220582ea4ae2a9789f35f6e887ec801b3ef
zi3lak <zi3lak@gmail.com>
2024-03-24 10:09:03 +0000

    Update app.vue

--- app.vue
+++ app.vue
@@ -3,3 +3,22 @@
     <h1>Piotr Klepuszewski</h1>
   </div>
 </template>
+<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div class="tradingview-widget-container__widget"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-mini-symbol-overview.js" async>
+  {
+  "symbol": "COINBASE:BTCUSD",
+  "width": 350,
+  "height": 220,
+  "locale": "en",
+  "dateRange": "1D",
+  "colorTheme": "dark",
+  "isTransparent": false,
+  "autosize": false,
+  "largeChartUrl": ""
+}
+  </script>
+</div>
+<!-- TradingView Widget END -->
