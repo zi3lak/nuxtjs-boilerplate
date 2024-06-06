ee3175825344ff5a5fa9c04dd590cbf3050d2dcd
zi3lak <zi3lak@gmail.com>
2024-06-06 16:11:22 +0100

    Update app.vue
    add 2

--- app.vue
+++ app.vue
@@ -4,21 +4,27 @@
     <p>Banks must be trusted to hold our money and transfer it electronically, but they lend it out in waves of credit bubbles with barely a fraction in reserve. We have to trust them with our privacy, trust them not to let identity thieves drain our accounts. Their massive overhead costs make micropayments impossible.</p>
   </div>
 
-<!-- TradingView Widget BEGIN -->
-<div class="tradingview-widget-container">   <div class="tradingview-widget-container__widget"></div>   <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>   <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-mini-symbol-overview.js" async>
-   {
+  <!-- TradingView Widget BEGIN -->
+  <div class="tradingview-widget-container">
+    <div class="tradingview-widget-container__widget"></div>
+    <div class="tradingview-widget-copyright">
+      <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
+        <span class="blue-text">Track all markets on TradingView</span>
+      </a>
+    </div>
+    <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-mini-symbol-overview.js" async>
+    {
       "symbol": "COINBASE:BTCUSD",
       "width": 350,
       "height": 220,
       "locale": "en",
-      "dateRange": "4h",
+      "dateRange": "1D",
       "colorTheme": "dark",
       "isTransparent": false,
       "autosize": false,
       "largeChartUrl": ""
-   }
-</script>
-<!-- TradingView Widget END -->
-  
+    }
+    </script>
+  </div>
+  <!-- TradingView Widget END -->
 </template>
-
