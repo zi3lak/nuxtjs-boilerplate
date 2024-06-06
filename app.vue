6e13903bb66b3c9eed2e69ecdf593935a75eba19
zi3lak <zi3lak@gmail.com>
2024-06-06 16:05:32 +0100

    Update app.vue
    add symbol

--- app.vue
+++ app.vue
@@ -5,23 +5,19 @@
   </div>
 
 <!-- TradingView Widget BEGIN -->
-<div class="tradingview-widget-container">
-  <div class="tradingview-widget-container__widget"></div>
-  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
-  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-mini-symbol-overview.js" async>
-  {
-  "symbol": "COINBASE:BTCUSD",
-  "width": 350,
-  "height": 220,
-  "locale": "en",
-  "dateRange": "1D",
-  "colorTheme": "dark",
-  "isTransparent": false,
-  "autosize": false,
-  "largeChartUrl": ""
-}
-  </script>
-</div>
+<div class="tradingview-widget-container">   <div class="tradingview-widget-container__widget"></div>   <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>   <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-mini-symbol-overview.js" async>
+   {
+      "symbol": "COINBASE:BTCUSD",
+      "width": 350,
+      "height": 220,
+      "locale": "en",
+      "dateRange": "4h",
+      "colorTheme": "dark",
+      "isTransparent": false,
+      "autosize": false,
+      "largeChartUrl": ""
+   }
+</script>
 <!-- TradingView Widget END -->
   
 </template>
